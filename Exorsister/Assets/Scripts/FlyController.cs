a3f525549f8da1bad065f8730926aaa3d60df568
Jeffery Kelly <jdk9162@rit.edu>
2016-01-29 22:31:46 -0500

    Flies Die When They Are Killed
    Flies are now destroyed when they are clicked thanks to new code in
    GameObjectController class.

--- Exorsister/Assets/Scripts/FlyController.cs
+++ Exorsister/Assets/Scripts/FlyController.cs
@@ -1,17 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class FlyController : MonoBehaviour {
+public class FlyController : GameObjectController {
     
     private float speed = 5.0f;
     private Vector3 forward = new Vector2(1, 0);
+    public static List<FlyController> flies;
 	// Use this for initialization
 	void Start () {
-	    
-	}
+	    if (flies == null)
+        {
+            flies = new List<FlyController>();
+        }
+        flies.Add(this);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position += forward * speed * Time.deltaTime;
+        //transform.position += forward * speed * Time.deltaTime;
+
+        if (IsBeingClicked())
+        {
+            FlyController.flies.Remove(this);
+            Destroy(gameObject);
+        }
 	}
 }
---
+++ Exorsister/Assets/Scripts/GameObjectController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameObjectController : MonoBehaviour {
+    
+    public bool IsBeingClicked()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Vector3 mp2 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(mp2);
+            mousePos.z = -1;
+            
+            if (Physics.Raycast(mousePos, new Vector3(0, 0, 1), out hit, float.MaxValue))
+            {
+                if (hit.collider == gameObject.GetComponent<Collider>())
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public bool IsMouseBeingDraggedOver()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            Vector3 mp2 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(mp2);
+            mousePos.z = -1;
+
+            if (Physics.Raycast(mousePos, new Vector3(0, 0, 1), out hit, float.MaxValue))
+            {
+                if (hit.collider == gameObject.GetComponent<Collider>())
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
