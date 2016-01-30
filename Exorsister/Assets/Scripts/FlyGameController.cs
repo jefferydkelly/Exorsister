fe38f8420e3f6d93c1de41234a377ec57d91ce19
Jeffery Kelly <jdk9162@rit.edu>
2016-01-30 08:01:41 -0500

    Toki wo Tomare!
    Time now counts down on the Fly Swatter game screen.  There is now a
    MinigameController class that has functions for handling win and loss.

--- Exorsister/Assets/Scripts/FlyGameController.cs
+++ Exorsister/Assets/Scripts/FlyGameController.cs
@@ -2,7 +2,8 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class FlyGameController : MonoBehaviour {
+public class FlyGameController : MinigameController {
+    public float timeRemaining = 60.0f;
     public Text timeRemainingText;
     public Text fliesRemainingText;
 	// Use this for initialization
@@ -12,6 +13,16 @@ public class FlyGameController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        timeRemaining -= Time.deltaTime;
+        timeRemainingText.text = "Time Remaining: " + Mathf.RoundToInt(timeRemaining);
         fliesRemainingText.text = "Flies Remaining: " + FlyController.flies.Count;
+        
+        if (FlyController.flies.Count == 0)
+        {
+            Win();
+        } else if (timeRemaining <= 0)
+        {
+            Lose();
+        }
 	}
 }
---
+++ Exorsister/Assets/Scripts/MinigameController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MinigameController : MonoBehaviour {
+
+    public string nextScene;
+
+    public void Win()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+
+    public void Lose()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+}
