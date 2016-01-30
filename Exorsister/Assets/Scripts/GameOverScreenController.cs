87000a6512b873425aad199d38731c8a84c673d0
unknown <al2303@IGMGRAD-02.main.ad.rit.edu>
2016-01-30 11:40:04 -0500

    Added user accessibility for previous versions of unity

--- Exorsister/Assets/Scripts/GameOverScreenController.cs
+++ Exorsister/Assets/Scripts/GameOverScreenController.cs
@@ -1,13 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+#if UNITY_5_3
 using UnityEngine.SceneManagement;
+#endif
 
 public class GameOverScreenController : MonoBehaviour {
 
 	public void Restart()
     {
-        SceneManager.LoadScene("title");
-    }
+#if UNITY_5_3
+		SceneManager.LoadScene("title");
+#else
+		Application.LoadLevel("title");
+#endif
+	}
 
     public void Quit()
     {
--- Exorsister/Assets/Scripts/StartMenuController.cs
+++ Exorsister/Assets/Scripts/StartMenuController.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+#if UNITY_5_3
 using UnityEngine.SceneManagement;
+#endif
 
 public class StartMenuController : MonoBehaviour {
     public string FirstGame = "Fly Swatter";
 	public void StartGame()
     {
-        SceneManager.LoadScene(FirstGame);
-    }
+#if UNITY_5_3
+		SceneManager.LoadScene(FirstGame);
+#else
+		Application.LoadLevel(FirstGame);
+#endif
+	}
 
-    public void QuitGame()
+	public void QuitGame()
     {
         Application.Quit();
     }
