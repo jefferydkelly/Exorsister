2def9cfb33ed0d7c36863f5902bc565519add6e6
Jeffery Kelly <jdk9162@rit.edu>
2016-01-29 21:46:02 -0500

    More files and flies
    Added in a few more files for organizing everything and added in basic
    flies too.

---
+++ Exorsister/Assets/Scripts/FlyController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyController : MonoBehaviour {
+    
+    private float speed = 5.0f;
+    private Vector3 forward = new Vector2(1, 0);
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += forward * speed * Time.deltaTime;
+	}
+}
