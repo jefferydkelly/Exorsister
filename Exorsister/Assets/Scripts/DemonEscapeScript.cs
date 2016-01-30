64b16d4756a6af8f33d01f0c2ddaad2fa4c54336
James Spinelli <DrX250@hotmail.com>
2016-01-30 00:42:04 -0500

    Added Holy Water Game
    Added Holy Water Game. Work in progress needs collisions working to
    detect when demon escapes. Also clicking to throw holy water not
    implemented yet.

---
+++ Exorsister/Assets/Scripts/DemonEscapeScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemonEscapeScript : MonoBehaviour {
+
+	int direction; // Number 0-7 for which direction he's heading
+	// Use this for initialization
+	void Start () 
+	{
+		direction = (int)Random.Range(0,8);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Translate (Vector3.right * Time.deltaTime);
+	}
+
+	void OnTriggerExit(Collider col)
+	{
+		print ("test");
+
+		//{
+			//End Game
+		//	print("Game Over");
+		//}
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		print ("test");
+	}
+}
