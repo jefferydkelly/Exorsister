6366b1e3cef8dca885cd572249764ca27b689a03
DrX250 <DrX250@hotmail.com>
2016-01-30 11:37:01 -0500

    DemonEscapeScript

--- Exorsister/Assets/Scripts/DemonEscapeScript.cs
+++ Exorsister/Assets/Scripts/DemonEscapeScript.cs
@@ -10,12 +10,41 @@ public class DemonEscapeScript : GameObjectController
 	void Start () 
 	{
 		direction = (int)Random.Range(0,8);
+		print (direction);
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		//transform.Translate (Vector3.right * Time.deltaTime);
+		/*switch(direction)
+		{
+		case 0:
+			transform.Translate (Vector3.right *Time.deltaTime);
+			break;
+		case 1:
+			transform.Translate (((Vector3.right + Vector3.up)/2) *Time.deltaTime);
+			break;
+		case 2:
+			transform.Translate (Vector3.up *Time.deltaTime);
+			break;
+		case 3:
+			transform.Translate (((Vector3.left + Vector3.up)/2) *Time.deltaTime);
+			break;
+		case 4:
+			transform.Translate (Vector3.left *Time.deltaTime);
+			break;
+		case 5:
+			transform.Translate (((Vector3.left + Vector3.down)/2) *Time.deltaTime);
+			break;
+		case 6:
+			transform.Translate (Vector3.down *Time.deltaTime);
+			break;
+		case 7:
+			transform.Translate (((Vector3.right + Vector3.down)/2) *Time.deltaTime);
+			break;
+		default:
+			print ("ERROR");
+		}*/
 	}
 
 	void OnTriggerExit(Collider col)
