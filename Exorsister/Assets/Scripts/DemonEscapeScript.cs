8185fe6f509590428a047d269413306e274a61b0
DrX250 <DrX250@hotmail.com>
2016-01-31 13:50:00 -0500

    Updated Holy water game

--- Exorsister/Assets/Scripts/DemonEscapeScript.cs
+++ Exorsister/Assets/Scripts/DemonEscapeScript.cs
@@ -10,16 +10,23 @@ public class DemonEscapeScript : GameObjectController
 	// Use this for initialization
 	private bool escaped = false;
 	float speedMod;
+
+	GameObject g;
+
 	void Start () 
 	{
 		direction = (int)Random.Range(0,8);
 		center = new Vector3 (0, 1, 0);
 		speedMod = 5.0f;
+		g = gameObject.GetComponent("GameObject").gameObject;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
+
+		int rotate = 0;
+
 		//Debug.Log (time);
 		time -= Time.deltaTime;
 		if (time > 0) 
@@ -49,31 +56,44 @@ public class DemonEscapeScript : GameObjectController
 			switch (direction) {
 			case 2:
 				transform.Translate ((Vector3.right * Time.deltaTime) * speedMod);
+				rotate = 90;
 				break;
 			case 1:
 				transform.Translate ((((Vector3.right + Vector3.up) / 2) * Time.deltaTime) * speedMod);
+				rotate = 45;
 				break;
 			case 0:
 				transform.Translate ((Vector3.up * Time.deltaTime) * speedMod);
+				rotate = 0;
 				break;
 			case 7:
 				transform.Translate ((((Vector3.left + Vector3.up) / 2) * Time.deltaTime) * speedMod);
+				rotate = 315;
 				break;
 			case 6:
 				transform.Translate ((Vector3.left * Time.deltaTime) * speedMod);
+				rotate = 270;
 				break;
 			case 5:
 				transform.Translate ((((Vector3.left + Vector3.down) / 2) * Time.deltaTime) * speedMod);
+				rotate = 225;
 				break;
 			case 4:
 				transform.Translate ((Vector3.down * Time.deltaTime) * speedMod);
+				rotate = 180;
 				break;
 			case 3:
 				transform.Translate ((((Vector3.right + Vector3.down) / 2) * Time.deltaTime) * speedMod);
+				rotate = 135;
 				break;
 			default:
 				print ("ERROR");
 				break;
+			}
+
+			if (rotate != null)
+			{
+				g.transform.Rotate(0,0,rotate);
 			}
 		}
 	}
