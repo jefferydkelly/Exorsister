7480104679a43912736f908d1f017234ac1ab5c8
Isaac Kelman (RIT Student) <imk6506@ad.rit.edu>
2016-01-30 20:30:41 -0500

    Commit

---
+++ Exorsister/Assets/Scripts/FinishScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinishScript : MonoBehaviour {
+    public int checkScore = 0;
+    public float timer = 20;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    public void increaseScore()
+    {
+        checkScore++;
+        if(checkScore == 5)
+        {
+            
+            winGame();
+        }
+    }
+
+    void winGame()
+    {
+
+    }
+
+    void looseGame()
+    {
+
+    }
+	// Update is called once per frame
+	void Update () {
+        
+        if(timer >= 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else { looseGame(); }
+	}
+}
