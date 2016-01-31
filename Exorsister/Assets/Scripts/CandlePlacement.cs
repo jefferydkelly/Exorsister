c16b3ff87660e8b69120a4a0a745878b38cb21ff
Jeffery Kelly <jdk9162@rit.edu>
2016-01-31 14:19:06 -0500

    Candle sfx is probably there but it's way too quiet.

--- Exorsister/Assets/Scripts/CandlePlacement.cs
+++ Exorsister/Assets/Scripts/CandlePlacement.cs
@@ -21,6 +21,7 @@ public class CandlePlacement : MonoBehaviour {
             Destroy(other.gameObject);
             test.GetComponent<FinishScript>().increaseScore();
             colorBox.GetComponent<_Color_Select_Script>().setColor();
+            Camera.main.GetComponent<AudioSource>().Play();
         }
     }
 
